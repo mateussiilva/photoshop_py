72492ebe444985228d8e460b04b5313dc09dd84f
mateussiilva <mateuspython3010@gmail.com>
2023-08-12 17:50:11 -0300

    OOP

--- photoshop.py
+++ photoshop.py
@@ -1,128 +1,45 @@
-from PyQt6.QtWidgets import (
-    QApplication,QMainWindow,QLabel,
-    QPushButton,QWidget,QToolBar,QScrollArea,QScrollBar,QVBoxLayout,)
-from PyQt6.QtGui import QAction, QIcon,QPixmap
-from PySimpleGUIQt import popup_get_file
-
-icone_criar_arquivo = "imagens/icons/icons8-crie-um-novo-16.png"
-icone_importar_arquivo = "imagens/icons/icons8-import-file-16.png"
-
-class ButtonFerramenta(QPushButton):
-    def __init__(self,filename,action) -> None:
-        super().__init__()
-        self.action = action
-        # self.setText(text)
-        self.setIcon(QIcon(filename))
-        self.setFixedSize(50,50)
-        self.clicked.connect(self.clique)
-
-    def clique(self):
-        print(f"Você clicou {self.action}")
-        
-
-
-class BarraFerramentas(QMainWindow):
-    def __init__(self):
-        super().__init__()
-        toolbar = QToolBar("Barra de ferramentas")
-        self.addToolBar(toolbar)
-        button_action = QAction("Your button", self)
-        button_action.triggered.connect(self.onMyToolBarButtonClick)
-        toolbar.addAction(button_action)
-        
-    def onMyToolBarButtonClick(self, s):
-        print("click", s)
+from PIL import Image
+import warnings
+import cv2
 
 
 
-class PhotoshopPy(QMainWindow):
-    def __init__(self,title_window:str,width:int,heigth:int) -> None:
-        super().__init__()
-        self.setWindowTitle(title_window)
-        self.setGeometry(0,0,width,heigth)
-        self.initUI()
-        # DEFINIÇÃO DOS WIDGETS
-        # layout = QVBoxLayout()
-        # toolbar = QToolBar("Barra de ferramentas")
-        # scroll_area = QScrollArea()
-        # widget_content = QWidget()
-        # vertical_scrollbar = QScrollBar()
-        # horizontal_scrollbar = QScrollBar()
-        # button_action = QAction("Abrir Arquivo",self)
-        
-        # button_action.triggered.connect(self.select_file)
-        # pixmap = QPixmap("fundo1.jpg")
-        # image_lbl = QLabel("")
-        # image_lbl.setPixmap(pixmap)
-
-        
-
-        # # CONFIGURANDO OS WIDGETS
-        # toolbar.addAction(button_action)
-
-        # scroll_area.setWidget(widget_content)
-        # scroll_area.setVerticalScrollBar(vertical_scrollbar)
-        # scroll_area.setHorizontalScrollBar(horizontal_scrollbar)       
-        
-        # layout.addWidget(image_lbl)
-        # widget_content.setLayout(layout)
+Image.MAX_IMAGE_PIXELS = None
 
 
-        # self.addToolBar(toolbar) 
-        # self.setCentralWidget(scroll_area)
+class Imagem:
+    def __init__(self,path_imagem) -> None:
+        self.path_imagem = path_imagem
+        self.__imagem = self.open_image_pillow(path_imagem)
 
-    def initUI(self):
-        self.setWindowTitle('Exemplo de QScrollArea')
-        self.setGeometry(100, 100, 800, 600)
 
-        # Criando o widget que será colocado na área de rolagem
-        widget_content = QWidget()
+    def open_image_pillow(self,path_image):
+        try:
+            img = Image.open(path_image) 
+        except: 
+            print("Error")
+        return img
 
-        # Layout vertical para o widget_content
-        layout = QVBoxLayout()
-
-        # Adicionando vários widgets (labels) ao layout
-        pixmap = QPixmap("fundo1.jpg")
-        image_lbl = QLabel("")
-        image_lbl.setPixmap(pixmap)
-
-        # Definindo o layout para o widget_content
-        layout.addWidget(image_lbl)
-        widget_content.setLayout(layout)
-
-        # Criando a área de rolagem e adicionando o widget_content a ela
-        scroll_area = QScrollArea()
-        scroll_area.setWidget(widget_content)
-
-        # Criando barras de rolagem vertical e horizontal
-        vertical_scrollbar = QScrollBar()
-        horizontal_scrollbar = QScrollBar()
-
-        # Configurando as barras de rolagem
-        scroll_area.setVerticalScrollBar(vertical_scrollbar)
-        scroll_area.setHorizontalScrollBar(horizontal_scrollbar)
-
-        # Definindo a área de rolagem como widget central da janela principal
-        self.setCentralWidget(scroll_area)
-    
-    def select_file(self):
-        path_file = popup_get_file("Selecione o arquivo")
-        return path_file
-
-    def set_image(self):
-        p_arquivo = self.select_file()
-
-    
+    def open_image_cv2(self,mostrar_img=False):
+        i = cv2.imread(self.path_imagem)
+        if mostrar_img:
+            cv2.imshow(" ",i)
+            while True:
+                ...
+        return i
+    def show_image(self):
+        i = self.__imagem
+        # thub = i.thumbnail((100,100))
+        return i.show()
 
 
 
 if __name__ == "__main__":
-    app = QApplication([])
+    caminho = "media/mateus/ARQUIVOS/PAINEESI"
+    p_imagem = "fundo1.tif"
+    image = Imagem(p_imagem)
+    img= image.open_image_cv2(True)
+    # cv2.imshow("teste",img)
     
-    window = PhotoshopPy("Photoshop Python",800,600)
-    # ferramentas = BarraFerramentas()
-    window.show()
-    # ferramentas.show()
 
-    app.exec()
-
+    
