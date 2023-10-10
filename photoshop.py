b360eaa45c908633f9d0c4100ea26d3069311dde
mateussiilva <mateuspython3010@gmail.com>
2023-10-10 18:25:30 -0300

    Removendo arquivos inuteis

--- photoshop.py
+++ photoshop.py
@@ -1,45 +1,11 @@
-from PIL import Image
-import warnings
-import cv2
 
 
 
-Image.MAX_IMAGE_PIXELS = None
-
-
-class Imagem:
-    def __init__(self,path_imagem) -> None:
-        self.path_imagem = path_imagem
-        self.__imagem = self.open_image_pillow(path_imagem)
-
-
-    def open_image_pillow(self,path_image):
-        try:
-            img = Image.open(path_image) 
-        except: 
-            print("Error")
-        return img
-
-    def open_image_cv2(self,mostrar_img=False):
-        i = cv2.imread(self.path_imagem)
-        if mostrar_img:
-            cv2.imshow(" ",i)
-            while True:
-                ...
-        return i
-    def show_image(self):
-        i = self.__imagem
-        # thub = i.thumbnail((100,100))
-        return i.show()
 
 
 
-if __name__ == "__main__":
-    caminho = "media/mateus/ARQUIVOS/PAINEESI"
-    p_imagem = "fundo1.tif"
-    image = Imagem(p_imagem)
-    img= image.open_image_cv2(True)
-    # cv2.imshow("teste",img)
-    
+
+paht_image = "/home/mateus/Imagens/fundo_cidade.jpg"
+
 
     
