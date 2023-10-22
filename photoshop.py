e9ad3ab30065e2d2d57efb5c607fce7b01b8b383
mateussiilva <mateuspython3010@gmail.com>
2023-10-21 22:49:13 -0300

    Rescrevendo a logica do projeto

--- photoshop.py
+++ photoshop.py
@@ -1,55 +1,21 @@
 from PIL import Image
-from os.path import getsize
-import pprint 
+import os
+import glob
 
 
+HOME = "/home/mateus/"
+
+PATH_SOURCE = os.path.join(HOME,"Imagens")
+
+def get_images(path,extensions=(".png",".jpeg",".jpg")):
+    for file in os.listdir(path):
+        p_comepleto = os.path.join(PATH_SOURCE,file)
+        if os.path.isfile(p_comepleto):
+            
+            print(file)
 
 
-class Imagem:
-    def __init__(self,path_imagem) -> None:
-        self.path_imagem = path_imagem
-        self.__imagem = self.open_image_pillow(path_imagem)
-
-    def open_image_pillow(self,path_image):
-        try:
-            img = Image.open(path_image) 
-        except: 
-            print("Error")
-        return img
-
-    def show_image(self):
-        i = self.__imagem
-        return i.show()
-    
-    def __get_size(self) -> tuple:
-        size = self.__imagem.size
-        return size
-    
-    def __get_resolution(self) -> tuple:
-        dpi = self.__imagem.info["dpi"]
-        return dpi
-
-    def __get_size_image(self):
-        size_file = getsize(self.path_imagem) 
-        return (round(size_file / 1024 ** 2,3),"MB")
-
-    def get_characteristics_for_image(self) -> dict:
-        return {
-            "tamanho_pixel": self.__get_size(),
-            "resolução_dpi":self.__get_resolution(),
-            "tamanho_arquivo": self.__get_size_image()
-        }
-        ...
-
-def main():
-    caminho = "media/mateus/ARQUIVOS/PAINEESI"
-    p_imagem = "fundo1.jpg"
-    image = Imagem(p_imagem)
-    pprint.pprint(image.get_characteristics_for_image())
 
 
 if __name__ == "__main__":
-    Image.MAX_IMAGE_PIXELS = None # ME PERMITE ESTRAPOLAR O LIMITE DEFINIDO PELO PILLOW !!CUIDADO
-    main()
-    
-    
+    get_images(PATH_SOURCE)
