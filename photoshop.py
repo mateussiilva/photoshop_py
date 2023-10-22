f35d60ade37401cf32a42202b26e07f4b63e29d1
mateussiilva <mateuspython3010@gmail.com>
2023-10-22 18:59:45 -0300

    Melhorando os projetos

--- photoshop.py
+++ photoshop.py
@@ -1,21 +1,39 @@
 from PIL import Image
 import os
-import glob
+import sys
 
 
-HOME = "/home/mateus/"
 
-PATH_SOURCE = os.path.join(HOME,"Imagens")
-
-def get_images(path,extensions=(".png",".jpeg",".jpg")) -> list:
+def get_images(path,extensions) -> list:
+    imagens = []
     for file in os.listdir(path):
-        p_comepleto = os.path.join(PATH_SOURCE,file)
+        p_comepleto = os.path.join(path,file)
         if os.path.isfile(p_comepleto):
-            
-            print(file)
+            nome,exte = os.path.splitext(file)
+            if exte in extensions:
+                imagens.append(p_comepleto)
+                
+    return imagens if len(imagens) > 0 else None
 
 
+def create_image():
+    pass
+
+def calc_new_height(width, height, new_width):
+    return round(new_width * height / width)
+
+
+def converter_pixel_cm(size_px,dpi):
+    return tuple(map(lambda x: 2.54 * (x / dpi),size_px)) 
 
 
 if __name__ == "__main__":
-    get_images(PATH_SOURCE)
+    Image.MAX_IMAGE_PIXELS = None
+    PATH_SOURCE = sys.argv[1]
+    EXTENSOES= (".png",".jpeg",".jpg")
+    imagens = get_images(PATH_SOURCE,(".tif"))
+    # print(imagens)
+    img = Image.open(imagens[1])
+    size = tuple(map(lambda x: round(x//2),converter_pixel_cm(img.size)))
+    img.resize(size)
+    img.save("redimensionada.jpg")
