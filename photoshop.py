f5bcb201894b6cdd726135cc25bd22e2f6a792a8
mateussiilva <mateuspython3010@gmail.com>
2023-10-21 22:51:35 -0300

    Escrevendo a função de pegar as imagens

--- photoshop.py
+++ photoshop.py
@@ -7,7 +7,7 @@ HOME = "/home/mateus/"
 
 PATH_SOURCE = os.path.join(HOME,"Imagens")
 
-def get_images(path,extensions=(".png",".jpeg",".jpg")):
+def get_images(path,extensions=(".png",".jpeg",".jpg")) -> list:
     for file in os.listdir(path):
         p_comepleto = os.path.join(PATH_SOURCE,file)
         if os.path.isfile(p_comepleto):
