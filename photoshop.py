6dc4c84a4a235686e5d5450b2831005892424dce
mateussiilva <mateuspython3010@gmail.com>
2024-01-27 17:04:50 -0300

    Implemetando o metodo de criação de imagem

--- photoshop.py
+++ photoshop.py
@@ -1,17 +1,38 @@
-from PIL import Image,ImageFilter
-import cv2
-import numpy as np
-import os
+from PIL import Image
+from os.path import join
+import rembg
 
 
-IMAGE_DIR = "images_teste"
-imagens = [os.path.join(IMAGE_DIR,image) for image in os.listdir(IMAGE_DIR)]
+Image.MAX_IMAGE_PIXELS = None
 
-filename = imagens[0]
 
-img = Image.open(filename)
-imgcv2 = cv2.imread(filename)
-w, h, c = imgcv2.shape
+class PhotoPy:
+    def __init__(self) -> None:
+        self.mode = "RGB"
 
-imgenhacent = img.filter(ImageFilter.GaussianBlur(45))
-imgenhacent.show() 
+    @classmethod
+    def open_image(cls, filename: str, isgray: bool = False) -> Image:
+
+        if isgray:
+            img = Image.open(filename)
+            return img.convert("L")
+
+        img = Image.open(filename)
+        return img
+
+    def criar_image(self, name: str, size: tuple, color: tuple = (255,255,255), extensao: str = ".jpg"):
+        i = Image.new(self.mode, size=size,color=color)
+        p = name + extensao
+        i.save(p)
+
+    def escreverimage(self, img, posicao,texto,colortext):
+        pass
+
+if __name__ == "__main__":
+    photopy = PhotoPy()
+    image = photopy.open_image(
+        "images_teste/wide-angle-shot-two-women-walking-beach-with-surfing-boards.jpg", isgray=False)
+    # image.show()
+    image_sem_fundo = rembg.remove(image)
+
+    image_sem_fundo.save("sem_fundo.png")
