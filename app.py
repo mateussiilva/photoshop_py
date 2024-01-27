7bf3d820113b0b5b5e4a46aea88259e5af8121ad
mateussiilva <mateuspython3010@gmail.com>
2024-01-27 17:05:26 -0300

    Esse é a nossa interface de controle dao PhotoPy

--- app.py
+++ app.py
@@ -1,30 +1,11 @@
-from PIL import Image
-import rembg
+from photoshop import PhotoPy
+from os.path import join
 
 
-Image.MAX_IMAGE_PIXELS = None
 
+IMAGES_DIR = "images_teste"
 
-class PhotoPy:
-    def __init__(self) -> None:
-        self.mode = "RGB"
-
-    @classmethod
-    def open_image(cls, filename: str, isgray: bool = False) -> Image:
-
-        if isgray:
-            img = Image.open(filename)
-            return img.convert("L")
-
-        img = Image.open(filename)
-        return img
-
-
-if __name__ == "__main__":
-    photopy = PhotoPy()
-    image = photopy.open_image(
-        "images_teste/wide-angle-shot-two-women-walking-beach-with-surfing-boards.jpg", isgray=False)
-    # image.show()
-    image_sem_fundo = rembg.remove(image)
-
-    image_sem_fundo.save("sem_fundo.png")
+photopy = PhotoPy()
+for n in range(300):
+    f = join(IMAGES_DIR,f"teste_{n}")
+    i = photopy.criar_image(f,(100,100),color=(100,25,n))
