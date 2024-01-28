a017b039d2658c64ceabee4b1c7fd56810d155f9
mateussiilva <mateuspython3010@gmail.com>
2024-01-28 17:48:05 -0300

    A parte de escrita em imagem vai ser separada nessa clase

---
+++ WriteImage.py
@@ -0,0 +1,15 @@
+from PIL import ImageFont,ImageDraw
+
+
+
+
+class WriteImage:
+    font = ""
+    def __init__(self):
+        pass        
+
+
+
+if __name__ == "__main__":
+    wimage = WriteImage()
+    
