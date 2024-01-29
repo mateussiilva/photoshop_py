b2d95c48a56b32ff10c5bb143da9b45aaec76fad
mateussiilva <mateuspython3010@gmail.com>
2024-01-28 22:56:07 -0300

    Tentando melhorar a gui

--- gui.py
+++ gui.py
@@ -1,22 +1,28 @@
 import PySimpleGUI as sg
 from photoshop import PhotoPy
 
+SIZE = 940, 800
+
 layout = [
     [
         sg.Text("Imagem:"), sg.Input(key="-PATH_IMAGE-"),
-        sg.FileBrowse("Open Image",key="-FILE_OPEN-")],
-    [sg.Button("Load Image",key="-LOAD-")],
+        sg.FileBrowse("Open Image", key="-FILE_OPEN-")
+    ],
+    [
+        sg.Button("Load Image", key="-LOAD-")
+    ],
     [sg.Text(key="-RES-")]
 ]
 
 
-window = sg.Window("PhotoPy", layout)
+window = sg.Window("PhotoPy", layout, size=SIZE)
 photopy = PhotoPy()
 while 1:
     events, values = window.read()
     if events == "-LOAD-":
-        image = values["-PATH_IMAGE-"]
-        dados = photopy.dados_imagem(photopy.open_image(image))
+        pathimage = values["-PATH_IMAGE-"]
+        image = photopy.open_image(pathimage)
+        dados = photopy.dados_imagem(image)
         window["-RES-"].update(dados)
     if events == sg.WINDOW_CLOSED:
         break
