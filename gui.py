e2b3c852908d0ac1f6a4e7230030714e9268e39f
mateussiilva <mateuspython3010@gmail.com>
2024-01-28 23:10:08 -0300

    Melhorando a interface do sistema

--- gui.py
+++ gui.py
@@ -6,10 +6,8 @@ SIZE = 940, 800
 layout = [
     [
         sg.Text("Imagem:"), sg.Input(key="-PATH_IMAGE-"),
-        sg.FileBrowse("Open Image", key="-FILE_OPEN-")
-    ],
-    [
-        sg.Button("Load Image", key="-LOAD-")
+        sg.FileBrowse("Open Image", key="-FILE_OPEN-"),
+        sg.Button("Load Image", key="-LOAD-"),
     ],
     [sg.Text(key="-RES-")]
 ]
@@ -19,7 +17,7 @@ window = sg.Window("PhotoPy", layout, size=SIZE)
 photopy = PhotoPy()
 while 1:
     events, values = window.read()
-    if events == "-LOAD-":
+    if events == "-LOAD-" or events == "-FILE_OPEN-":
         pathimage = values["-PATH_IMAGE-"]
         image = photopy.open_image(pathimage)
         dados = photopy.dados_imagem(image)
