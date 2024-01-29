4d80d832c0827c2640555a085a4a7677bd05d8e1
mateussiilva <mateuspython3010@gmail.com>
2024-01-28 23:37:49 -0300

    Separando em frames as areas da nossa interface

--- gui.py
+++ gui.py
@@ -1,7 +1,12 @@
 import PySimpleGUI as sg
+from PySimpleGUI import Text, Checkbox, Radio
 from photoshop import PhotoPy
 
 SIZE = 940, 800
+
+frame_dados_imagem = [
+    [Text(key="-RES-")]
+]
 
 layout = [
     [
@@ -9,19 +14,27 @@ layout = [
         sg.FileBrowse("Open Image", key="-FILE_OPEN-"),
         sg.Button("Load Image", key="-LOAD-"),
     ],
-    [sg.Text(key="-RES-")]
+    [sg.Frame(title="Dados Imagem",layout=frame_dados_imagem)]
 ]
 
 
 window = sg.Window("PhotoPy", layout, size=SIZE)
 photopy = PhotoPy()
 while 1:
+    # Ler os eventos e os valores da JANELA
     events, values = window.read()
+
+    # Carregar a imagem e suas informações
     if events == "-LOAD-" or events == "-FILE_OPEN-":
         pathimage = values["-PATH_IMAGE-"]
         image = photopy.open_image(pathimage)
         dados = photopy.dados_imagem(image)
         window["-RES-"].update(dados)
+        
+    
+
+        
+    # Fechar a Janela 
     if events == sg.WINDOW_CLOSED:
         break
 
